333ae9d17fb74ed9099dd62d0b9faf8931f7b00a
Kazuki <shanbenkazuki15@gmail.com>
2023-07-27 23:39:25 +0900

    user新規登録のCRUD作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'top#index'
   get 'privacy_policy', to: 'top#privacy_policy'
   get 'terms_of_use', to: 'top#terms_of_use'
+  # root to: 'users#index'
+  resources :users
 end
