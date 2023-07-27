b8bfe1a307fcdbe5192c6179cd8a906a57995bb9
Kazuki <shanbenkazuki15@gmail.com>
2023-07-28 00:08:27 +0900

    ログイン画面のCRUD作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'top#index'
   get 'privacy_policy', to: 'top#privacy_policy'
   get 'terms_of_use', to: 'top#terms_of_use'
-  # root to: 'users#index'
   resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
