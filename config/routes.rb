6b8fc9a405f75ea9d7cde5ce379c54f7a7c8e128
Kazuki Yamamoto <74257571+shanbenkazuki@users.noreply.github.com>
2023-08-01 16:32:25 +0900

    commit (#51)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root "top#index"
+  root 'top#index'
+  get 'privacy_policy', to: 'top#privacy_policy'
+  get 'terms_of_use', to: 'top#terms_of_use'
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
