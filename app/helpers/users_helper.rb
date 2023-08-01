b50fd801f6187e4ff106693f186702df32bf8e3c
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-02 00:50:47 +0900

    ユーザ登録とログイン機能のrspec追加

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+  def user_form_button
+    case params[:action]
+    when 'new'
+      '登録'
+    when 'edit'
+      'Update'
+    when 'create'
+      '登録'
+    when 'update'
+      'Update'
+    else
+      raise 'unexpected action'
+    end
+  end
 end
