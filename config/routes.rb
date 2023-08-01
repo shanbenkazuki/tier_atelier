bb427a894a6dfa96a71c37bcd8b80f94785dba20
Kazuki Yamamoto <74257571+shanbenkazuki@users.noreply.github.com>
2023-08-01 17:14:09 +0900

    ユーザとログインの機能作成 (#53)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   root 'top#index'
   get 'privacy_policy', to: 'top#privacy_policy'
   get 'terms_of_use', to: 'top#terms_of_use'
-  resources :users
-
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   post 'logout' => 'user_sessions#destroy', :as => :logout
