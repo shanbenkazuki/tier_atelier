d98208432646984b38351bb27510061a1cbe0727
Kazuki Yamamoto <74257571+shanbenkazuki@users.noreply.github.com>
2023-08-02 21:14:20 +0900

    Create tier new (#63)
    * search画面をi18n対応
    
    * カテゴリ表示用のデータをseedデータとして登録
    
    * Tierの設定画面の作成
    
    * make用の画面追加
    
    * ローカライズ対応

--- app/controllers/tiers_controller.rb
+++ app/controllers/tiers_controller.rb
@@ -1,5 +1,9 @@
 class TiersController < ApplicationController
   def index; end
 
+  def new
+    @categories = Category.all
+  end
+
   def search; end
 end
--- app/helpers/tiers_helper.rb
+++ app/helpers/tiers_helper.rb
@@ -1,2 +1,16 @@
 module TiersHelper
+  def tier_form_button
+    case params[:action]
+    when 'new'
+      '作成'
+    when 'edit'
+      'Update'
+    when 'create'
+      '登録'
+    when 'update'
+      'Update'
+    else
+      raise 'unexpected action'
+    end
+  end
 end
