ea34355da2a6e8d1c98970f80d940c69aa6cd2c5
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-03 19:13:56 +0900

    画像以外テーブルに保存できるようにした

--- app/controllers/tiers_controller.rb
+++ app/controllers/tiers_controller.rb
@@ -3,11 +3,33 @@ class TiersController < ApplicationController
 
   def new
     @categories = Category.all
+    @tier_list = TierList.new
   end
 
   def create
+    ActiveRecord::Base.transaction do
+      @tier_list = current_user.tier_lists.create!(tier_list_params)
+      1.upto(5) do |i|
+        @tier_list.tiers.create!(horizontal_name: params["tier_list"]["horizontal_label_#{i}"], vertical_name: params["tier_list"]["vertical_label_#{i}"], horizontal_order: i, vertical_order: i)
+      end
+      # if params[:images]
+      #   params[:images].each do |image|
+      #     @tier_list.items.create!(name: image.original_filename, image: image, order: 1) 
+      #   end
+      # end
+    end
+    
     redirect_to make_tiers_path
+  rescue => e
+    flash[:error] = "Failed to create tier list: #{e.message}"
+    render :new
   end
 
   def search; end
+
+  private
+
+  def tier_list_params
+    params.require(:tier_list).permit(:category_id, :title, :description)
+  end
 end
--- app/models/tier_list.rb
+++ app/models/tier_list.rb
@@ -1,4 +1,6 @@
 class TierList < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  has_many :tiers
 end
