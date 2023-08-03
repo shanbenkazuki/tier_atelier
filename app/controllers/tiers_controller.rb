99a5bbff81bada38612052e9e4738e5fc4f77dd4
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-04 00:50:25 +0900

    tier設定画面からshowへ、本番環境設定変更

--- app/controllers/tiers_controller.rb
+++ app/controllers/tiers_controller.rb
@@ -8,15 +8,13 @@ class TiersController < ApplicationController
 
   def create
     ActiveRecord::Base.transaction do
-      # binding.pry
       @tier_list = current_user.tier_lists.create!(tier_list_params)
       1.upto(5) do |i|
         @tier_list.tiers.create!(horizontal_name: params["tier_list"]["horizontal_label_#{i}"], vertical_name: params["tier_list"]["vertical_label_#{i}"], horizontal_order: i, vertical_order: i)
       end
     end
-    redirect_to make_tiers_path, success: t('.success')
+    redirect_to tier_path(@tier_list), success: t('.success')
   rescue => e
-    # binding.pry
     @categories = Category.all
     flash.now[:danger] = t('.fail')
     render :new, status: :unprocessable_entity
