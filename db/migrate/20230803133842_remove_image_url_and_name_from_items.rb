82a70e386e65dd268eed192815de07856d4ecdc1
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-03 23:08:25 +0900

    itemテーブル削除

--- db/migrate/20230803133842_remove_image_url_and_name_from_items.rb
+++
@@ -1,6 +0,0 @@
-class RemoveImageUrlAndNameFromItems < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :items, :image_url, :string
-    remove_column :items, :name, :string
-  end
-end
