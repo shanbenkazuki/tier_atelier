433f275ed96cfb98a041bae9ee6b3b79e57be7fb
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-09 17:11:11 +0900

    jquery,jquery-uiを導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,22 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 
-import $ from 'jquery';
-window.$ = $;
-window.jQuery = $;
+import "./src/jquery"
+import "./src/jquery-ui"
 
+$(function() {
+  $('.draggable').draggable({
+    revert: "invalid",
+    start: function(event, ui) {
+      $(this).data('originalPosition', $(this).position());
+    }
+  });
+  
+  $('#dropzone').droppable({
+    accept: '.draggable',
+    drop: function(event, ui) {
+      ui.draggable.css(ui.draggable.data('originalPosition'));
+      $(this).append(ui.draggable);
+    }
+  });
+});
