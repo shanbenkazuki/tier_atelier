7db6ccbd01f545a8c88ba9dcf78b1eb7fa7c75ac
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-09 14:17:51 +0900

    jqueryを追加

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
+
