ee842d940ac6ed0f7a9ec098b3ccc0ec1724f00b
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-10 10:41:08 +0900

    ドラッグアンドドロップ実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,23 +2,80 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import '@fortawesome/fontawesome-free'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(fas, far, fab)
 
 import "./src/jquery"
 import "./src/jquery-ui"
 
 $(function() {
   $('.draggable').draggable({
-    revert: "invalid",
-    start: function(event, ui) {
-      $(this).data('originalPosition', $(this).position());
+    revert: 'invalid',
+    helper: 'clone',
+    appendTo: 'body'
+  });
+
+  $('.tier.cell').droppable({
+    accept: '.draggable',
+    drop: function(event, ui) {
+      // 元の画像を削除
+      ui.draggable.remove();
+  
+      // ドロップされた要素のクローンを作成
+      var cloned = $(ui.helper).clone();
+  
+      // クローンの位置をリセット
+      cloned.css({
+        top: 0,
+        left: 0,
+        position: 'relative'
+      });
+  
+      // クローンをドラッグ可能に設定
+      cloned.draggable({
+        revert: 'invalid',
+        helper: 'clone',
+        appendTo: 'body'
+      });
+  
+      // クローンをドロップ先に追加
+      $(this).append(cloned);
+    }
+  });  
+
+  // containerをドロップ先として設定
+  $('.container').droppable({
+    accept: '.draggable',
+    drop: function(event, ui) {
+      // 元の画像を削除
+      ui.draggable.remove();
+  
+      // ドロップされた要素のクローンを作成
+      var cloned = $(ui.helper).clone();
+  
+      // クローンの位置をリセット
+      cloned.css({
+        top: 0,
+        left: 0,
+        position: 'relative'
+      });
+  
+      // クローンをドラッグ可能に設定
+      cloned.draggable({
+        revert: 'invalid',
+        helper: 'clone',
+        appendTo: 'body'
+      });
+  
+      // クローンをドロップ先に追加
+      $(this).append(cloned);
     }
   });
   
-  $('#dropzone').droppable({
-    accept: '.draggable',
-    drop: function(event, ui) {
-      ui.draggable.css(ui.draggable.data('originalPosition'));
-      $(this).append(ui.draggable);
-    }
-  });
 });
+
+
