ba32d118d0be8e51c4ab8267862c6ed3f74fb9da
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-09-29 00:48:29 +0900

    更新修正

--- app/controllers/tiers_controller.rb
+++ app/controllers/tiers_controller.rb
@@ -27,15 +27,15 @@ class TiersController < ApplicationController
     ActiveRecord::Base.transaction do
       @tier = current_user.tiers.create!(tier_params)
       # 初期値を保存する
-      @tier.tier_categories.create!(name: params["tier"]["default_rank"], order: 0)
-      @tier.tier_ranks.create!(name: params["tier"]["default_category"], order: 0)
+      @tier.tier_categories.create!(name: params["tier"]["default_category"], order: 0)
+      @tier.tier_ranks.create!(name: params["tier"]["default_rank"], order: 0)
       category_column_num = params["tier"]["category_column_num"].to_i
       rank_column_num = params["tier"]["rank_column_num"].to_i
       1.upto(category_column_num) do |i|
-        @tier.tier_categories.create!(name: params["tier"]["category_#{i-1}"], order: i)
+        @tier.tier_categories.create!(name: params["tier"]["category_#{i}"], order: i)
       end
       1.upto(rank_column_num) do |i|
-        @tier.tier_ranks.create!(name: params["tier"]["rank_#{i-1}"], order: i)
+        @tier.tier_ranks.create!(name: params["tier"]["rank_#{i}"], order: i)
       end
       # 画像の数だけItemテーブルに保存する
       params[:tier][:images].each do |image|
@@ -71,14 +71,14 @@ class TiersController < ApplicationController
         
         # Update or create TierCategories
         params["tier"]["category_column_num"].to_i.times do |i|
-          name = params["tier"]["category_#{i}"]
+          name = params["tier"]["category_#{i + 1}"]
           tier_category = @tier.tier_categories.find_or_initialize_by(order: i + 1)
           tier_category.update!(name: name)
         end
   
         # Update or create TierRanks
         params["tier"]["rank_column_num"].to_i.times do |i|
-          name = params["tier"]["rank_#{i}"]
+          name = params["tier"]["rank_#{i + 1}"]
           tier_rank = @tier.tier_ranks.find_or_initialize_by(order: i + 1)
           tier_rank.update!(name: name)
         end
