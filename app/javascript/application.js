ffb97777c468ddb6f93d8e07d86d87e768b4f464
Kazuki Yamamoto <74257571+shanbenkazuki@users.noreply.github.com>
2023-10-05 14:07:06 +0900

    モーダルをjqueryUIからVanillaJSに変更 (#159)
    * モーダル表示
    
    * モーダルをvanillaに変更
    
    * モーダルをjqueryからvanillaJSに変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,6 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import * as ActiveStorage from "@rails/activestorage"
-import html2canvas from 'html2canvas'
 import '@fortawesome/fontawesome-free'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
@@ -11,33 +10,10 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { library } from "@fortawesome/fontawesome-svg-core";
 library.add(fas, far, fab)
 
-import "./src/jquery"
-import "./src/jquery-ui"
 import "./src/direct_uploads"
 import "./src/tier_item_updater"
 import "./src/dynamic_field"
 import "./src/tier_image_uploader"
+import "./src/save_tier_image_modal"
 
 ActiveStorage.start()
-
-$(function() {
-  $("#myModal").dialog({
-    autoOpen: false,
-    modal: true,
-    buttons: {
-      "ダウンロード": function() {
-        html2canvas(document.querySelector("#tier-container"), { 
-          useCORS: true 
-        }).then(canvas => {
-          var link = document.createElement('a');
-          link.href = canvas.toDataURL("image/png");
-          link.download = 'tier-container.png';
-          link.click();
-        });
-      },
-      "Close": function() {
-        $(this).dialog("close");
-      }
-    }
-  });
-});
