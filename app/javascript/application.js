cd5000b7910f8c5e481ad91f8ba7fe29b85d7dd3
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-10-05 12:15:41 +0900

    モーダル表示

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -20,24 +20,48 @@ import "./src/tier_image_uploader"
 
 ActiveStorage.start()
 
-$(function() {
-  $("#myModal").dialog({
-    autoOpen: false,
-    modal: true,
-    buttons: {
-      "ダウンロード": function() {
-        html2canvas(document.querySelector("#tier-container"), { 
-          useCORS: true 
-        }).then(canvas => {
-          var link = document.createElement('a');
-          link.href = canvas.toDataURL("image/png");
-          link.download = 'tier-container.png';
-          link.click();
-        });
-      },
-      "Close": function() {
-        $(this).dialog("close");
-      }
+document.addEventListener("DOMContentLoaded", function() {
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("openModal");
+  var span = document.getElementById("closeModal");
+
+  // ボタンクリックでモーダルを開く
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // × マークをクリックでモーダルを閉じる
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // モーダルの外側をクリックでモーダルを閉じる
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
-  });
+  }
+});
+
+
+$(function() {
+  // $("#myModal").dialog({
+  //   autoOpen: false,
+  //   modal: true,
+  //   buttons: {
+  //     "ダウンロード": function() {
+  //       html2canvas(document.querySelector("#tier-container"), { 
+  //         useCORS: true 
+  //       }).then(canvas => {
+  //         var link = document.createElement('a');
+  //         link.href = canvas.toDataURL("image/png");
+  //         link.download = 'tier-container.png';
+  //         link.click();
+  //       });
+  //     },
+  //     "Close": function() {
+  //       $(this).dialog("close");
+  //     }
+  //   }
+  // });
 });
