c1becc1da3adb7a94b1dd50e6beaeb631f162f82
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-10-05 16:08:41 +0900

    テスト作成の途中

--- app/javascript/src/save_tier_image_modal.js
+++ app/javascript/src/save_tier_image_modal.js
@@ -3,15 +3,17 @@ import html2canvas from 'html2canvas'
 document.addEventListener("DOMContentLoaded", () => {
   const downloadBtn = document.getElementById("download-image-button");
 
-  // ダウンロードボタンの動作
-  downloadBtn.addEventListener('click', () => {
-    html2canvas(document.querySelector("#tier-container"), { 
-      useCORS: true 
-    }).then(canvas => {
-      const link = document.createElement('a');
-      link.href = canvas.toDataURL("image/png");
-      link.download = 'tier-container.png';
-      link.click();
+  if (downloadBtn) {
+    // ダウンロードボタンの動作
+    downloadBtn.addEventListener('click', () => {
+      html2canvas(document.querySelector("#tier-container"), { 
+        useCORS: true 
+      }).then(canvas => {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL("image/png");
+        link.download = 'tier-container.png';
+        link.click();
+      });
     });
-  });
+  }
 });
