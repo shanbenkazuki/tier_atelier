dbf0d4f8ce7b800fbc5aefaf780480ef53fbc91f
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-10-11 13:54:29 +0900

    log消し忘れ

--- app/javascript/src/direct_uploads.js
+++ app/javascript/src/direct_uploads.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   document.addEventListener("direct-upload:initialize", event => {
     const { target, detail } = event
     const { id, file } = detail
-    console.log(target)
     target.insertAdjacentHTML("beforebegin", `
       <div id="direct-upload-${id}" class="direct-upload direct-upload--pending">
         <div id="direct-upload-progress-${id}" class="direct-upload__progress" style="width: 0%"></div>
