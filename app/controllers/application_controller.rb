5b3a0a41a82e52ac7b41594c18a5b60a40937ee3
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-10-11 14:56:04 +0900

    テスト環境でurl_optionsをつける

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
+  before_action :set_active_storage_url_options
+
+  private
+
+  def not_authenticated
+    redirect_to login_path, danger: "ログインしてください"
+  end
+
+  def set_active_storage_url_options
+    if Rails.env.test?
+      ActiveStorage::Current.url_options = { protocol: 'http://', host: 'example.com', port: 80 }
+    end
+  end
 end
