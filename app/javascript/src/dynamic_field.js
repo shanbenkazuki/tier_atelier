6cb9663f20f25cd876caebae8463dc57a0dabc9b
Kazuki Yamamoto <74257571+shanbenkazuki@users.noreply.github.com>
2023-10-13 12:29:18 +0900

    削除ボタンを押してもフィールドが消えない問題を解消 (#177)

--- app/javascript/src/dynamic_field.js
+++ app/javascript/src/dynamic_field.js
@@ -1,12 +1,16 @@
-let categoryCounter = 5;
-let rankCounter = 5;
+const defaultFieldsNum = 5
+let categoryCounter = defaultFieldsNum;
+let rankCounter = defaultFieldsNum;
 const maxFields = 10;
+const rankPlaceholders = [ "E", "F", "G", "H", "I"];
+const categoryPlaceholders = [ "Balance", "Speeder", "Defender", "Supporter", "Attacker"];
 
-function addField(event, counter, max, idPrefix, alertMaxMessage) {
+function addField(event, counter, max, idPrefix, alertMaxMessage, placeholders) {
     if (counter < max) {
+        const placeholder = placeholders[counter - defaultFieldsNum] || "";
         counter++;
         const newTextField = `
-            <input class="form-control mt-2" type="text" name="tier[tier_${idPrefix}_attributes][${counter}][name]" id="tier_tier_${idPrefix}_attributes_${counter}_name">
+            <input class="form-control mt-2" type="text" name="tier[tier_${idPrefix}_attributes][${counter}][name]" id="tier_tier_${idPrefix}_attributes_${counter}_name" placeholder="${placeholder}">
             <input value="${counter}" autocomplete="off" type="hidden" name="tier[tier_${idPrefix}_attributes][${counter}][order]" id="tier_tier_${idPrefix}_attributes_${counter}_order">`;
         event.target.insertAdjacentHTML('beforebegin', newTextField);
     } else {
@@ -15,11 +19,17 @@ function addField(event, counter, max, idPrefix, alertMaxMessage) {
     return counter;
 }
 
+
 function removeField(counter, min, idPrefix, alertMessage) {
     if (counter > min) {
-        const elementToRemove = document.getElementById(`tier_${idPrefix}_${counter}`);
-        if (elementToRemove) {
-            elementToRemove.parentNode.removeChild(elementToRemove);
+        const inputNameToRemove = document.getElementById(`tier_tier_${idPrefix}_attributes_${counter}_name`);
+        const inputOrderToRemove = document.getElementById(`tier_tier_${idPrefix}_attributes_${counter}_order`);
+
+        if (inputNameToRemove) {
+            inputNameToRemove.parentNode.removeChild(inputNameToRemove);
+        }
+        if (inputOrderToRemove) {
+            inputOrderToRemove.parentNode.removeChild(inputOrderToRemove);
         }
         counter--;
     } else {
@@ -31,13 +41,13 @@ function removeField(counter, min, idPrefix, alertMessage) {
 function handleBodyClick(event) {
     switch (event.target.id) {
         case 'add-category':
-            categoryCounter = addField(event, categoryCounter, maxFields, 'categories', '最大カテゴリ数に達しました');
+            categoryCounter = addField(event, categoryCounter, maxFields, 'categories', '最大カテゴリ数に達しました', categoryPlaceholders);
             break;
         case 'remove-category':
             categoryCounter = removeField(categoryCounter, 5, 'categories', 'これ以上フィールドを削除できません');
             break;
         case 'add-rank':
-            rankCounter = addField(event, rankCounter, maxFields, 'ranks', '最大ランク数に達しました');
+            rankCounter = addField(event, rankCounter, maxFields, 'ranks', '最大ランク数に達しました', rankPlaceholders);
             break;
         case 'remove-rank':
             rankCounter = removeField(rankCounter, 5, 'ranks', 'これ以上フィールドを削除できません');
