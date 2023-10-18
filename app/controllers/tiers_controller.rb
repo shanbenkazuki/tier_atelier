69c3c5711a96154cd6506cc8d420a49a9ed0982b
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-10-18 13:00:08 +0900

    リファクタリング

--- app/controllers/tiers_controller.rb
+++ app/controllers/tiers_controller.rb
@@ -1,42 +1,13 @@
 class TiersController < ApplicationController
   include ApplicationHelper
   before_action :set_categories, only: [:new, :edit]
-  before_action :set_tier, only: [:edit, :show, :destory]
+  before_action :set_tier, only: [:edit, :show, :destroy, :make]
   before_action :require_login
 
   def index; end
 
   def show
-    tier_categories = TierCategory.where(tier_id: @tier.id).order(:order)
-    tier_ranks = TierRank.where(tier_id: @tier.id).order(:order)
-
-    @category_name_and_ids = tier_categories.non_zero.pluck(:name, :id)
-    @rank_name_and_ids = tier_ranks.non_zero.pluck(:name, :id)
-
-    @category_id_with_order_zero = tier_categories.find_by(order: 0)&.id
-    @rank_id_with_order_zero = tier_ranks.find_by(order: 0)&.id
-
-    @items = Item.where(tier_id: @tier.id)
-
-    @tier_colors = Rails.application.config.tier_colors
-
-    @images_map = {}
-
-    @items.each do |item|
-      key = if item.tier_rank_id == @rank_id_with_order_zero && item.tier_category_id == @category_id_with_order_zero
-              "uncategorized_unranked"
-            else
-              "#{item.tier_rank_id}_#{item.tier_category_id}"
-            end
-      variant = item.image.variant(resize_to_limit: [60, nil]).processed
-      image_data = {
-        url: url_for(variant.url),
-        id: item.id
-      }
-
-      @images_map[key] ||= []
-      @images_map[key] << image_data
-    end
+    setup_tier
   end
 
   def new
@@ -48,28 +19,12 @@ class TiersController < ApplicationController
   def edit; end
 
   def create
-    ActiveRecord::Base.transaction do
-      @tier = current_user.tiers.create!(tier_params)
-      @tier.add_images(params[:tier][:images])
-    end
-    redirect_to make_tier_path(@tier), success: t('.success')
-  rescue ActiveRecord::RecordInvalid => e
-    handle_error("Validation Error: #{e.record.errors.full_messages.to_sentence}", Tier.new(tier_params))
-  rescue StandardError => e
-    handle_error("ERROR: #{e.message}\n#{e.backtrace.join("\n")}", Tier.new(tier_params))
+    save_tier(:create!)
   end
 
   def update
-    ActiveRecord::Base.transaction do
-      @tier = current_user.tiers.find(params[:id])
-      @tier.update!(tier_params)
-      @tier.add_images(params[:tier][:images])
-    end
-    redirect_to make_tier_path(@tier), success: t('.success')
-  rescue ActiveRecord::RecordInvalid => e
-    handle_error("Validation Error: #{e.record.errors.full_messages.to_sentence}", @tier)
-  rescue StandardError => e
-    handle_error("ERROR: #{e.message}\n#{e.backtrace.join("\n")}", @tier)
+    set_tier_for_update
+    save_tier(:update!)
   end
 
   def destroy
@@ -81,8 +36,6 @@ class TiersController < ApplicationController
   end
 
   def make
-    @tier = Tier.find(params[:id])
-
     set_meta_tags title: @tier.title,
                   og: {
                     image: url_for(@tier.cover_image.blob&.url)
@@ -90,37 +43,7 @@ class TiersController < ApplicationController
                   twitter: {
                     image: url_for(@tier.cover_image.blob&.url)
                   }
-
-    tier_categories = TierCategory.where(tier_id: @tier.id).order(:order)
-    tier_ranks = TierRank.where(tier_id: @tier.id).order(:order)
-
-    @category_name_and_ids = tier_categories.non_zero.pluck(:name, :id)
-    @rank_name_and_ids = tier_ranks.non_zero.pluck(:name, :id)
-
-    @category_id_with_order_zero = tier_categories.find_by(order: 0)&.id
-    @rank_id_with_order_zero = tier_ranks.find_by(order: 0)&.id
-
-    @items = Item.where(tier_id: @tier.id)
-
-    @tier_colors = Rails.application.config.tier_colors
-
-    @images_map = {}
-
-    @items.each do |item|
-      key = if item.tier_rank_id == @rank_id_with_order_zero && item.tier_category_id == @category_id_with_order_zero
-              "unranked_uncategorized"
-            else
-              "#{item.tier_rank_id}_#{item.tier_category_id}"
-            end
-      variant = item.image.variant(resize_to_limit: [60, nil]).processed
-      image_data = {
-        url: url_for(variant.url),
-        id: item.id
-      }
-
-      @images_map[key] ||= []
-      @images_map[key] << image_data
-    end
+    setup_tier
   end
 
   def search; end
@@ -133,23 +56,9 @@ class TiersController < ApplicationController
       :title,
       :description,
       :cover_image,
-      tier_ranks_attributes: [
-        :id,
-        :name,
-        :order,
-        :_destroy
-      ],
-      tier_categories_attributes: [
-        :id,
-        :name,
-        :order,
-        :_destroy
-      ]
+      tier_ranks_attributes: [:id, :name, :order, :_destroy],
+      tier_categories_attributes: [:id, :name, :order, :_destroy]
     )
-  end
-
-  def tier_category_params
-    params.require(:tier).permit(:name, :order)
   end
 
   def item_params
@@ -164,12 +73,52 @@ class TiersController < ApplicationController
     @tier = Tier.find(params[:id])
   end
 
-  def handle_error(error_message, tier)
+  def set_tier_for_update
+    @tier = current_user.tiers.find(params[:id])
+  end
+
+  def save_tier(method)
+    ActiveRecord::Base.transaction do
+      if method == :create!
+        @tier = Tier.create_with_images(current_user, tier_params, params[:tier][:images])
+      else
+        @tier.update_with_images(tier_params, params[:tier][:images])
+      end
+    end
+    redirect_to make_tier_path(@tier), success: t('.success')
+  rescue ActiveRecord::RecordInvalid => e
+    handle_tier_error("Validation Error: #{e.record.errors.full_messages.to_sentence}")
+  rescue StandardError => e
+    handle_tier_error("ERROR: #{e.message}\n#{e.backtrace.join("\n")}")
+  end
+
+  def handle_tier_error(error_message)
     Rails.logger.error error_message
-    @tier = tier
-    @categories = Category.all
+    @tier ||= Tier.new(tier_params)
+    @categories ||= Category.all
     flash.now[:danger] = t('.fail')
-    action_name = tier.new_record? ? :new : :edit
+    action_name = @tier.new_record? ? :new : :edit
     render action_name, status: :unprocessable_entity
+  end
+
+  def setup_tier
+    @category_labels = @tier.tier_categories.non_zero.pluck(:name, :id)
+    @rank_labels = @tier.tier_ranks.non_zero.pluck(:name, :id)
+
+    @uncategorized_tier_category_id = @tier.category_with_order_zero&.id
+    @unranked_tier_rank_id = @tier.rank_with_order_zero&.id
+
+    @items = @tier.items
+
+    @tier_colors = Rails.application.config.tier_colors
+
+    @images_map = {}
+
+    @items.each do |item|
+      variant_url = url_for(item.image.variant(resize_to_limit: [60, nil]).processed.url)
+      rank_category_key = item.generate_rank_category_key(@unranked_tier_rank_id, @uncategorized_tier_category_id)
+      @images_map[rank_category_key] ||= []
+      @images_map[rank_category_key] << item.image_data(variant_url)
+    end
   end
 end
