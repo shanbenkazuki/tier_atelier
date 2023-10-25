4da4f265635cf41dae0d329d0568cb28fd2f9e48
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-10-25 19:13:49 +0900

    マイアカウントにし、アバターとログアウトボタンを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :tiers, dependent: :destroy
   has_many :templates, dependent: :destroy
 
+  has_one_attached :avatar
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
