74efb0c4531599bdd7fc6716db564ce0e53f89f3
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-10-25 13:04:52 +0900

    リファクタリングとlocalstorageが空の場合は直接詳細画面に遷移する

--- app/javascript/controllers/arrange_tier_modal_controller.js
+++ app/javascript/controllers/arrange_tier_modal_controller.js
@@ -2,43 +2,62 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   updateItemsFromLocalStorage() {
+    const allImageData = this.collectImageDataFromLocalStorage();
+    const tierId = this.getAttributeFromElement(document, "#tier-container", "data-tier-id");
+
+    if (allImageData.length > 0) {
+      this.postImageDataToServer(allImageData, tierId);
+    } else {
+      this.redirectToTierPage(tierId);
+    }
+  }
+
+  collectImageDataFromLocalStorage() {
     const allImageData = [];
     const keysToRemove = [];
-  
+
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i);
       if (key.startsWith('imageData-')) {
         const storedData = localStorage.getItem(key);
         if (storedData) {
-          const parsedData = JSON.parse(storedData);
-          
-          // keyからitem_idを取得
-          const itemId = key.split('imageData-')[1];
-          parsedData.item_id = itemId;
-  
+          const parsedData = this.parseImageData(storedData, key);
           allImageData.push(parsedData);
           keysToRemove.push(key);
         }
       }
     }
 
-    const tierId = this.getAttributeFromElement(document, "#tier-container", "data-tier-id");
+    this.removeKeysFromLocalStorage(keysToRemove);
+    return allImageData;
+  }
 
+  parseImageData(storedData, key) {
+    const parsedData = JSON.parse(storedData);
+    const itemId = key.split('imageData-')[1];
+    parsedData.item_id = itemId;
+    return parsedData;
+  }
+
+  removeKeysFromLocalStorage(keysToRemove) {
+    for (const key of keysToRemove) {
+      localStorage.removeItem(key);
+    }
+  }
+
+  postImageDataToServer(imageData, tierId) {
     fetch(`/tiers/${tierId}/items/bulk_update_items`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Requested-With': 'XMLHttpRequest',
-          'X-CSRF-Token': this.getCsrfToken()
-        },
-        body: JSON.stringify(allImageData)
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-Token': this.getCsrfToken()
+      },
+      body: JSON.stringify(imageData)
     })
     .then(response => {
       if (response.ok) {
-        for (const key of keysToRemove) {
-          localStorage.removeItem(key);
-        }
-        window.location.href = response.url;
+        this.redirectToTierPage(tierId);
       } else {
         throw new Error('Network response was not ok');
       }
@@ -46,6 +65,10 @@ export default class extends Controller {
     .catch(error => {
       console.error('There was a problem with the fetch operation:', error.message);
     });
+  }
+
+  redirectToTierPage(tierId) {
+    window.location.href = `http://localhost:3000/tiers/${tierId}`;
   }
 
   getCsrfToken() {
