5e60b5f84d534e8180cc3b90448f71ab69d3c4fc
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-10-28 00:00:23 +0900

    バリデーションを増やす

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,19 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validate :avatar_size_validation, if: -> { avatar.attached? }
+  validates :name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :password, length: { minimum: 3, maximum: 16 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は英数字のみ設定してください' }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  validates :email, uniqueness: true, presence: true
+  private
+
+  def avatar_size_validation
+    if avatar.blob.byte_size > 1.megabyte
+      avatar.purge
+      errors.add(:avatar, 'は、1MB以下のサイズにしてください')
+    end
+  end
 end
