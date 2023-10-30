33a6396e5601c19d6d13fde7aab3be11be3d6ba0
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-10-30 11:17:05 +0900

    画像の５枚上限撤廃

--- app/javascript/controllers/item_count_checker_controller.js
+++ app/javascript/controllers/item_count_checker_controller.js
@@ -8,8 +8,6 @@ export default class extends Controller {
     if (files.length > 30) {
       alert("画像は30枚までしかアップロードできません。");
       this.fileTarget.value = '';
-    } else if (files.length < 5) {
-      alert("画像は5枚以上アップロードしてください。");
     }
   }
 }
