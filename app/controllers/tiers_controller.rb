60b375979dd0d6271c5dbd68182378312f9ccb40
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-12-06 23:04:56 +0900

    新規作成画面にログインしていなくても入れるようにする

--- app/controllers/tiers_controller.rb
+++ app/controllers/tiers_controller.rb
@@ -2,7 +2,7 @@ class TiersController < ApplicationController
   include ApplicationHelper
   before_action :set_categories, only: [:new, :edit]
   before_action :set_tier, only: [:edit, :show, :destroy, :arrange, :update_tier_cover_image]
-  before_action :require_login, except: [:index]
+  before_action :require_login, except: [:index, :new]
   before_action :authorize_tier, only: [:edit, :update, :destroy, :arrange, :update_tier_cover_image]
 
   def index
