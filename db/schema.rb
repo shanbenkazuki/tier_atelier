653e2040282ec61cf058ad7a3470392a5e7a1fb7
Kazuki Yamamoto <74257571+shanbenkazuki@users.noreply.github.com>
2023-12-13 22:52:09 +0900

    カテゴリの変更 (#304)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_06_110637) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_13_134321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_06_110637) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
-    t.string "cover_image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
