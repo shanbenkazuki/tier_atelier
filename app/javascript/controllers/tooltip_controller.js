4834df2b12640a5ce713f1a66010d7b64bc3c542
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-12-25 23:03:42 +0900

    tooltipのリファクタリング

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -1,12 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
-import * as bootstrap from 'bootstrap';
+import { Tooltip } from "bootstrap"
 
 export default class extends Controller {
   connect() {
-    new bootstrap.Tooltip(this.element, {
-      title: this.element.getAttribute("data-tooltip"),
-      placement: 'top',
-      trigger: 'hover focus'
+    new Tooltip(this.element, {
+      title: this.element.getAttribute("data-tooltip")
     });
   }
 }
